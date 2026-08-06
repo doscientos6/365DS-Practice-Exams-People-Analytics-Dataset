dfa79f974340d18361be328283de416dce2aa62e
Lau <vgallardolautaro@gmail.com>
2026-08-06 11:42:06 -0300

    Update 04_business_questions.sql

--- 01_query-sql/04_business_questions.sql
+++ 01_query-sql/04_business_questions.sql
@@ -1,1 +1,5 @@
-
+¿Qué departamento paga mejor?
+¿Qué departamento tiene más empleados?
+¿Qué puestos reciben mejores salarios?
+¿Qué managers administran más personas?
+¿Qué empleados están por encima del promedio salarial de su área?
