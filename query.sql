76fe70c7e7f51481ad29c98ab4a35df28566c68f
Lau <vgallardolautaro@gmail.com>
2026-08-06 00:16:23 -0300

    query.sql

--- query.sql
+++ query.sql
@@ -21,6 +21,34 @@ CREATE TABLE employees (
     hire_date DATE NOT NULL
 );
 
+-- TABLA: salaries
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+
+    PRIMARY KEY (emp_no, from_date),
+
+    FOREIGN KEY (emp_no)
+        REFERENCES employees(emp_no)
+);
+
+-- TABLA: titles
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+
+    PRIMARY KEY (emp_no, title, from_date),
+
+    FOREIGN KEY (emp_no)
+        REFERENCES employees(emp_no)
+);
+
 -- TABLA: dept_emp
 
 CREATE TABLE dept_emp (
@@ -55,34 +83,6 @@ CREATE TABLE dept_manager (
         REFERENCES departments(dept_no)
 );
 
--- TABLA: salaries
-
-CREATE TABLE salaries (
-    emp_no INT NOT NULL,
-    salary INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-
-    PRIMARY KEY (emp_no, from_date),
-
-    FOREIGN KEY (emp_no)
-        REFERENCES employees(emp_no)
-);
-
--- TABLA: titles
-
-CREATE TABLE titles (
-    emp_no INT NOT NULL,
-    title VARCHAR(50) NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE,
-
-    PRIMARY KEY (emp_no, title, from_date),
-
-    FOREIGN KEY (emp_no)
-        REFERENCES employees(emp_no)
-);
-    
 -- Realizar:
         SELECT *
         FROM "La tabla cargada"
