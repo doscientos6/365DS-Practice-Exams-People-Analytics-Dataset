6e2f92cb73b507355791fb237f02b50268f4a301
Lau <vgallardolautaro@gmail.com>
2026-08-06 22:31:57 -0300

    Update 04_business_questions.sql

--- 01_query-sql/04_business_questions.sql
+++ 01_query-sql/04_business_questions.sql
@@ -19,9 +19,12 @@ SELECT
     ROUND(AVG(DATE_PART('year', AGE(CURRENT_DATE, e.hire_date)))) AS avg_tenure_years,
     ROUND(AVG(s.salary)) AS average_salary
 FROM departments d
-JOIN dept_emp de ON d.dept_no = de.dept_no
-JOIN salaries s ON de.emp_no = s.emp_no
-JOIN employees e ON de.emp_no = e.emp_no
+JOIN dept_emp de 
+	ON d.dept_no = de.dept_no
+JOIN salaries s 
+	ON de.emp_no = s.emp_no
+JOIN employees e
+	ON de.emp_no = e.emp_no
 WHERE de.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 GROUP BY d.dept_name
 ORDER BY average_salary DESC;
@@ -33,8 +36,10 @@ SELECT
     SUM(s.salary) AS total_payroll,
     ROUND(AVG(s.salary)) AS average_salary
 FROM departments d
-JOIN dept_emp de ON d.dept_no = de.dept_no
-JOIN salaries s ON de.emp_no = s.emp_no
+JOIN dept_emp de 
+	ON d.dept_no = de.dept_no
+JOIN salaries s 
+	ON de.emp_no = s.emp_no
 WHERE de.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 GROUP BY d.dept_name
 ORDER BY total_payroll DESC;
